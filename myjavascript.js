74eac87264f1987f4eaa727814796af2a0798c9d
vietqvo <viet.vo@emichats.com>
2018-05-09 00:02:08 +1000

    add

--- myjavascript.js
+++ myjavascript.js
@@ -1,7 +1,19 @@
+var f = document.createElement("form");
+f.setAttribute('method',"post");
+f.setAttribute('action',"submit.php");
 
-var popup = open("", "Popup", "width=300,height=200");
-var txtOk = popup.document.createElement("TEXTAREA");
-var aOk = popup.document.createElement("a");
-aOk.innerHTML = "Click here";
-popup.document.body.appendChild(txtOk);
-popup.document.body.appendChild(aOk);
+var i = document.createElement("input"); //input element, text
+i.setAttribute('type',"text");
+i.setAttribute('name',"username");
+
+var s = document.createElement("input"); //input element, Submit button
+s.setAttribute('type',"submit");
+s.setAttribute('value',"Submit");
+
+f.appendChild(i);
+f.appendChild(s);
+
+//and some more input elements here
+//and dont forget to add a submit button
+
+document.getElementsByTagName('body')[0].appendChild(f);
