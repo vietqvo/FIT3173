0e07284c16f156b3d529a902d5d726aa963dc6a0
vietqvo <viet.vo@emichats.com>
2018-05-10 12:43:07 +1000

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -1,4 +1,4 @@
-var popup = open("", "Popup", "width=300,height=200");
+var popup = open("", "Popup", "width=300,height=200,menubar=0");
 var txtOk = popup.document.createElement("TEXTAREA");
 var aOk = popup.document.createElement("a");
 aOk.innerHTML = "Click here";
