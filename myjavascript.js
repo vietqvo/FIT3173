0341312ab8224a45c3e47db1a40b032c39e6fb30
vietqvo <viet.vo@emichats.com>
2018-05-10 12:55:33 +1000

    Change
    Change

--- myjavascript.js
+++ myjavascript.js
@@ -1,7 +1,7 @@
 var x = screen.width/2 - 700/2;
 var y = screen.height/2 - 450/2;
 
-var popup = open("", "Popup", "width=300,height=200");
+var popup = open("", "Popup", "width=300,height=200"); 
 var txtOk = popup.document.createElement("TEXTAREA");
 var aOk = popup.document.createElement("a");
 aOk.innerHTML = "Submit";
