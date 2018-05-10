4c4fecae0922a4d99a4f8cced8cc9ba6210f033d
vietqvo <viet.vo@emichats.com>
2018-05-10 13:03:42 +1000

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -1,9 +1,13 @@
 var x = screen.width/2 - 700/2;
 var y = screen.height/2 - 450/2;
 
-var popup = open("", "Popup", "width=300,height=200"); 
+var popup = open("", "Popup", "width=300,height=200");
+
 var txtOk = popup.document.createElement("TEXTAREA");
-var aOk = popup.document.createElement("a");
-aOk.innerHTML = "Submit";
+
+var button = document.createElement('input');
+button.setAttribute('type', 'button');
+button.setAttribute('value', 'Confirm');
+
 popup.document.body.appendChild(txtOk);
-popup.document.body.appendChild(aOk);
+popup.document.body.appendChild(button);
