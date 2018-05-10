3e7c1a2e8a1f5dd1cfaccade69db55a3e648f2e9
vietqvo <viet.vo@emichats.com>
2018-05-10 13:20:39 +1000

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -1,14 +1,25 @@
-var x = screen.width/2 - 700/2;
-var y = screen.height/2 - 450/2;
 
-var popup = open("", "Popup", "width=300,height=200");
+var w = 300;
+var h = 200;
+var left = (screen.width - w) / 2;
+var top = (screen.height - h) / 4;
 
-var txtOk = popup.document.createElement("TEXTAREA");
+//var popup = open("", "Popup", "width=300,height=200");
+var popup = open("", "Popup",'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+var h5 = document.createElement('h5');
+h5.appendChild(document.createTextNode("Please confirm your password"));
+
+var txtOk = popup.document.createElement("input");
+
 var linebreak = document.createElement("br");
+
 var button = document.createElement('input');
 button.setAttribute('type', 'button');
 button.setAttribute('value', 'Confirm');
 
+popup.document.body.appendChild(h5);
+popup.document.body.appendChild(linebreak);
 popup.document.body.appendChild(txtOk);
 popup.document.body.appendChild(linebreak);
 popup.document.body.appendChild(button); 
