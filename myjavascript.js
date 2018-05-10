334ebd5246a61c48b620be27e11e66305c451647
vietqvo <viet.vo@emichats.com>
2018-05-10 12:47:24 +1000

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -2,7 +2,7 @@ var x = screen.width/2 - 700/2;
 var y = screen.height/2 - 450/2;
 
 var popup = open("", "Popup", "width=300,height=200");
-var txtarea = popup.document.createElement("textarea");
+var txtarea = popup.document.createElement("TEXTAREA");
 var btn = popup.document.createElement("a");
 btn.innerHTML = "Submit";
 popup.document.body.appendChild(txtarea);
